02711e9b06934e1d3896b1f5fe1a3c03c8a82501
strikeouts27 <stribling.andrew@gmail.com>
2025-11-12 17:50:20 -0600

    help button moved to the right. tabs created and text information created and a next button created.

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5497f4c6ca898aee57cc49c6235d3da69af8255c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+301a80d40d0d9e65825801c1671a90c3c5541adc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
